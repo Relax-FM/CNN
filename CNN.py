4fe8028b22c4925b35c1dc3e33146d1213467adb
Relax_FM <arton.2@mail.ru>
2023-11-06 18:27:18 +0300

    Add some files, fixed bugs

--- CNN.py
+++ CNN.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.cuda.amp import autocast, GradScaler
 import torchvision as tv
 import cv2
 
@@ -9,17 +9,30 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import time
+import yaml
 import datetime
 
 from DataSet import DataSet2Class
 from NN import ConvNet
 from Functions import *
+from Losses import get_losser
+from Optimizer import get_optimizer
 
+options_path = 'config.yml'
+with open(options_path, 'r') as options_stream:
+    options = yaml.safe_load(options_stream)
 
-train_dogs_path = 'C:/Users/relax_fm/PycharmProjects/CNN/dataset/training_set/dogs'
-train_cats_path = 'C:/Users/relax_fm/PycharmProjects/CNN/dataset/training_set/cats'
-test_dogs_path = './dataset/test_set/dogs'
-test_cats_path = './dataset/test_set/cats'
+network_options = options.get('network')
+dataset_options = options.get('dataset')
+train_loader = dataset_options.get('train_loader')
+test_loader = dataset_options.get('test_loader')
+file_names = dataset_options.get('file_names')
+
+train_dogs_path = file_names.get('train_dogs_path')
+train_cats_path = file_names.get('train_cats_path')
+test_dogs_path = file_names.get('test_dogs_path')
+test_cats_path = file_names.get('test_cats_path')
 
 train_ds_catsdogs = DataSet2Class(train_dogs_path, train_cats_path)
 test_ds_catsdogs = DataSet2Class(test_dogs_path, test_cats_path)
@@ -32,12 +45,14 @@ test_ds_catsdogs = DataSet2Class(test_dogs_path, test_cats_path)
 batch_size = 16
 
 train_loader = torch.utils.data.DataLoader(
-    train_ds_catsdogs, shuffle=True,
-    batch_size=batch_size, num_workers=0, drop_last=True
+    train_ds_catsdogs, shuffle=train_loader.get('shuffle'),
+    batch_size=train_loader.get('batch_size'), num_workers=train_loader.get('num_workers'),
+    drop_last=train_loader.get('drop_last')
 )
 test_loader = torch.utils.data.DataLoader(
-    train_ds_catsdogs, shuffle=True,
-    batch_size=batch_size, num_workers=0, drop_last=False
+    test_ds_catsdogs, shuffle=test_loader.get('shuffle'),
+    batch_size=test_loader.get('batch_size'), num_workers=test_loader.get('num_workers'),
+    drop_last=test_loader.get('drop_last')
 )
 
 CNNet = ConvNet()
@@ -50,10 +65,14 @@ CNNet = ConvNet()
 #     print(CNNet(img))
 #     break
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(CNNet.parameters(), lr=0.0001, betas=(0.9, 0.999))
+loss_fn = get_losser(network_options.get('loss'))
+optimizer = get_optimizer(CNNet.parameters(), network_options.get('optimizer'))
 
-epochs = 20
+device = network_options.get('device')
+use_amp = network_options.get('use_amp')
+epochs = network_options.get('epochs')
+
+start_time = time.time()
 
 for epoch in range(epochs):
     loss_val = 0
@@ -63,11 +82,10 @@ for epoch in range(epochs):
         optimizer.zero_grad()
 
         lbl = F.one_hot(lbl, 2).float()
-        pred = CNNet(img)
 
-        # print(pred)
-        # print(lbl)
-        loss = loss_fn(pred, lbl)
+        with autocast(use_amp, dtype=torch.float16):
+            pred = CNNet(img)
+            loss = loss_fn(pred, lbl)
 
         loss.backward()
         loss_item = loss.item()
@@ -83,7 +101,9 @@ for epoch in range(epochs):
     print(f"Loss : {loss_val / len(train_loader)}")
     print(f"Acc : {acc_val / len(train_loader)}")
 
+print(f'Full time learning : {time.time() - start_time}')
+
 curDate = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
 print(curDate)
-path_name = ""+curDate+".pth"
+path_name = "" + curDate + "_" + device.upper() + ".pth"
 torch.save(CNNet.state_dict(), path_name)
